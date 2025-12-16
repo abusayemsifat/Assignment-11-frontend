92734028ed78836d3bc3577ae83ca31e28fe964f
Abu Sayem Sifat <sifatabusayem@gmail.com>
2025-12-16 21:54:41 +0600

    Added role based Authentication

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithPopup } f
 import React, { createContext, useEffect, useState } from 'react';
 import auth from '../Firebase/firebase.config';
 import { GoogleAuthProvider } from 'firebase/auth';
+import axios from 'axios';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext();
@@ -12,32 +13,47 @@ const AuthProvider = ({ children }) => {
 
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState(null);
+    const [role, setRole] = useState('');
 
     const registerWithEmailPassword = (email, pass) => {
         return createUserWithEmailAndPassword(auth, email, pass)
     }
 
-    const handleGoogleSignin = () =>{
+    const handleGoogleSignin = () => {
         return signInWithPopup(auth, googleProvider)
     }
 
-        useEffect(() => {
-            const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-                setUser(currentUser)
-                setLoading(false)
-            })
-            return () => {
-                unsubscribe()
-            }
 
-        }, [])
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        return () => {
+            unsubscribe()
+        }
+
+    }, [])
+
+    useEffect(() => {
+        if (!user) return;
+        axios.get(`http://localhost:5000/users/role/${user.email}`)
+            .then(res => {
+                setRole(res.data.role)
+            })
+    }, [user])
+
+    console.log(role)
 
     const authData = {
         registerWithEmailPassword,
         setUser,
         user,
         handleGoogleSignin,
-        loading
+        loading,
+        role,
     }
 
     return (
